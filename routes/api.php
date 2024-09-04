c45097e20f4f557f07dc6d2bd4d309144034a3ba
COLLINS <collincity111@gmail.com>
2024-09-04 12:13:34 +0100

    Defined API routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::apiResource('projects', ProjectController::class);
+
+Route::apiResource('projects.employees', EmployeeController::class);
+
+Route::post('projects/{project}/employees/{employee}/restore', 
+   [EmployeeController::class, 'restore'])
+   ->name('employees.restore');
