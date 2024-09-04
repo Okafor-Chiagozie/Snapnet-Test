ad474805602cfe2eca6de8f60b5282964f2e3a1b
COLLINS <collincity111@gmail.com>
2024-09-04 12:26:37 +0100

    Installed Laravel breeze

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,8 @@
 <?php
 
-use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
-
-
-Route::apiResource('projects', ProjectController::class);
-
-Route::apiResource('projects.employees', EmployeeController::class);
-
-Route::post('projects/{project}/employees/{employee}/restore', 
-   [EmployeeController::class, 'restore'])
-   ->name('employees.restore');
+});
