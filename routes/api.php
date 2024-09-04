791c62ee9c0435b30b5338cfc8ed79bc2afd4d9e
COLLINS <collincity111@gmail.com>
2024-09-04 12:29:42 +0100

    Added authentication to the API routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,23 @@
 <?php
 
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+   return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+
+   Route::apiResource('projects', ProjectController::class);
+
+   Route::apiResource('projects.employees', EmployeeController::class);
+
+   Route::post(
+      'projects/{project}/employees/{employee}/restore',
+      [EmployeeController::class, 'restore']
+   )
+      ->name('employees.restore');
 });
