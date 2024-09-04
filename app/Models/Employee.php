cd51d2d1d34240915f367d7c8d293a6ffc99b789
COLLINS <collincity111@gmail.com>
2024-09-04 11:23:38 +0100

    Setup models and defined relationships

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Employee extends Model
 {
-    use HasFactory;
+   use HasFactory, SoftDeletes;
+
+   protected $fillable = [
+      'project_id',
+      'name',
+      'email',
+      'position'
+   ];
+
+   public function project()
+   {
+      return $this->belongsTo(Project::class);
+   }
 }
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Project extends Model
 {
-    use HasFactory;
+   use HasFactory, SoftDeletes;
+
+   protected $fillable = [
+      'name',
+      'description',
+      'status',
+      'start_date',
+      'end_date'
+   ];
+
+   public function employees()
+   {
+      return $this->hasMany(Employee::class);
+   }
 }
