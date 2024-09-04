b96494a2f5e8fffd223e34fd28c9026a14ee160c
COLLINS <collincity111@gmail.com>
2024-09-04 11:58:05 +0100

    Created API endpoints for Project model

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class EmployeeController extends Controller
+{
+   
+}
---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ProjectController extends Controller
+{
+   /**
+    * Display a listing of the resource.
+    */
+   public function index()
+   {
+      return Project::with('employees')->get();
+   }
+
+   /**
+    * Store a newly created resource in storage.
+    */
+   public function store(Request $request)
+   {
+      $validator = Validator::make($request->all(), [
+         'name' => 'required|unique:projects,name',
+         'description' => 'nullable|string',
+         'status' => 'required|string',
+         'start_date' => 'required|date',
+         'end_date' => 'nullable|date',
+      ]);
+
+      if ($validator->fails()) {
+         return response()->json($validator->errors(), 422);
+      }
+
+      $project = Project::create($request->all());
+
+      return response()->json($project, 201);
+   }
+
+   /**
+    * Display the specified resource.
+    */
+   public function show($id)
+   {
+      $project = Project::with('employees')->findOrFail($id);
+
+      return response()->json($project);
+   }
+
+   /**
+    * Update the specified resource in storage.
+    */
+   public function update(Request $request, $id)
+   {
+      $project = Project::findOrFail($id);
+
+      $validator = Validator::make($request->all(), [
+         'name' => 'required|unique:projects,name,' . $project->id,
+         'description' => 'nullable|string',
+         'status' => 'required|string',
+         'start_date' => 'required|date',
+         'end_date' => 'nullable|date',
+      ]);
+
+      if ($validator->fails()) {
+         return response()->json($validator->errors(), 422);
+      }
+
+      $project->update($request->all());
+
+      return response()->json($project);
+   }
+
+   /**
+    * Remove the specified resource from storage.
+    */
+   public function destroy($id)
+   {
+      $project = Project::findOrFail($id);
+      $project->delete();
+
+      return response()->json(null, 204);
+   }
+}
